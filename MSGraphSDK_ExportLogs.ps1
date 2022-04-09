ab8e5414227d9452af78c2c9335ccaa9e06e0290
Jason Fritts <38323403+jasonfritts@users.noreply.github.com>
2022-04-09 10:16:16 -0400

    Update MSGraphSDK_ExportLogs.ps1

--- MSGraphSDK_ExportLogs.ps1
+++ MSGraphSDK_ExportLogs.ps1
@@ -1,6 +1,6 @@
 # NOTE: First install MSGraph PowerShell module See: https://docs.microsoft.com/en-us/graph/powershell/installation﻿
 
-$tId = "12345678-5ead-468c-a6ae-048e103d57f0"  # Add tenant ID from Azure Active Directory page on portal.
+$tId = "12345678-5ead-468c-a6ae-048e103d57f0"  # Add tenant ID from Azure Active Directory overview page on portal.azure.com
 $agoDays = 7  # Will filter the log for $agoDays from the current date and time. 
 $startDate = (Get-Date).AddDays(-($agoDays)).ToString('yyyy-MM-dd')  # Get filter start date.
 $pathForExport = "./"  # The path to the local filesystem for export of the CSV file.
@@ -8,11 +8,13 @@ $pathForExport = "./"  # The path to the local filesystem for export of the CSV 
 Connect-MgGraph -Scopes "AuditLog.Read.All" -TenantId $tId  # Or use Directory.Read.All.
 Select-MgProfile "beta"  #
 
-# Define the filtering strings. Below is example of finding all the Legacy Auth signins 
+# Define the filtering strings. Below is a example filter of finding all the Legacy Auth signins
+# For help on filters see https://docs.microsoft.com/en-us/graph/query-parameters#filter-parameter
+# To download ALL logs, do not define a filter and remove the -Filter flag from line 16 below.
 $filter = "createdDateTime ge $startDate and (clientAppUsed eq 'AutoDiscover' or clientAppUsed eq 'Exchange ActiveSync' or clientAppUsed eq 'Exchange Online PowerShell' or clientAppUsed eq 'Exchange Web Services' or clientAppUsed eq 'IMAP4' or clientAppUsed eq 'MAPI Over HTTP' or clientAppUsed eq 'Offline Address Book' or clientAppUsed eq 'Other clients' or clientAppUsed eq 'Outlook Anywhere (RPC over HTTP)' or clientAppUsed eq 'POP3' or clientAppUsed eq 'Reporting Web Services' or clientAppUsed eq 'Authenticated SMTP' or clientAppUsed eq 'Outlook Service')"
 
 # Get the interactive and non-interactive sign-ins based on filtering clauses.
-$signInsInteractive = Get-MgAuditLogSignIn -Filter ($filter) -All
+$signIns = Get-MgAuditLogSignIn -Filter ($filter) -All
 
 
 $columnList = @{  # Enumerate the list of properties to be exported to the CSV files.
@@ -21,7 +23,7 @@ $columnList = @{  # Enumerate the list of properties to be exported to the CSV f
                "ResourceDisplayName", "ResourceId", "UserAgent"
 }
 
-$signInsInteractive | Select-Object @columnList | Export-Csv -Path ($pathForExport + "LegacyAuthSignIns_$tId.csv") -NoTypeInformation
+$signIns | Select-Object @columnList | Export-Csv -Path ($pathForExport + "LegacyAuthSignIns_$tId.csv") -NoTypeInformation
 
 
 
